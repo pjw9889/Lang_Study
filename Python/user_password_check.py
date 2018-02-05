90ea2632f5b1a2d7cf26220903af385b9c6d40f7
Jae Park <pjw9889@gmail.com>
2018-02-05 15:29:20 +0900

    pass checking module

---
+++ Python/user_password_check.py
@@ -0,0 +1,42 @@
+import sys
+
+def password_validation_check(pwd):
+    """ checking password validation
+
+    Args:
+        pwd (str) : password string
+
+    Return :
+        True or False (boolean) : the result of checking validation
+    """
+    if len(pwd) < 6 or len(pwd) >12:
+        print(pwd, 'have length problem')
+        return False
+
+    for x in pwd:
+        if not x.isnumeric() and not x.isalpha():
+            print(pwd, 'dont consist of english and number')
+            return False
+
+    upper = False
+    lower = False
+
+    for x in pwd:
+        if upper and lower:
+            break
+
+        if x.isalpha():
+            if not upper:
+                upper = x.isupper()
+            if not lower:
+                lower = x.islower()
+
+    if not upper or not lower:
+        print(pwd, 'dont have together upper and lower')
+        return False
+
+    print(pwd, 'is correct password')
+    return True
+
+    if __name__ == '__main__':
+        password_validation_check(sys.argv[1])
